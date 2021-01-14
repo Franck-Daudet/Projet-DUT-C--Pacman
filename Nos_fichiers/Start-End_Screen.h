6f492a7df77508e6597fa4ce2ceac619f4147799
Daudet <franck.daudet@etu.univ-amu.fr>
2021-01-14 15:00:28 +0100

    Update deplacement.h

--- Nos_fichiers/Start-End_Screen.h
+++ Nos_fichiers/Start-End_Screen.h
@@ -72,5 +72,3 @@ int End_Screen()
     }
 
 }
-
-
--- main.cpp
+++ main.cpp
@@ -14,21 +14,5 @@ using namespace std;
 
 int main()
 {
-    while(true)
-    {
-        int x;
-        int y;
-        x = Start_Screen();
-        if (x==1)
-            Launch_Game();
-        // if (x==2) -> classment
-        // if (x==3) -> settings
-        if (x==4) return 0;
-        y = End_Screen();
-        if (y==1)
-            Launch_Game();
-        // if (y==2) -> classment
-        // if (y==3) -> settings
-        if (y==4) return 0;
-    }
+    Launch_Game();
 }
