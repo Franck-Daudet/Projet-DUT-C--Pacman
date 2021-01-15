ef7699f3f964d9136e3dc448e9deb5ee74325736
J.Nicolas <nicolas.jaubert@etu.univ-amu.fr>
2021-01-15 15:24:13 +0100

    Rajout des couleurs sur les anims (enfin)

--- Nos_fichiers/Start-End_Screen.h
+++ Nos_fichiers/Start-End_Screen.h
@@ -17,40 +17,60 @@
 #include<unistd.h>
 
 
+
 using namespace std;
 
+const string Kreset   ("0");
+const string Kyellow   ("33");
+const string Kred   ("31");
+const string Kwhite ("37");
+const string KBwhite ("47");
 
+void Color2 (const string & coul)
+{
+    cout << "\033[" << coul <<"m";
+}
 
 void ClearScreen2() /*Clear the screen*/
 {
     cout << "\033[H\033[2J";
 }
 
-void LoadScreen(const string& link)
+void LoadScreen(const string& Link, const string& TheColor)
 {
     ClearScreen2();
-    ifstream Flow(link);
-    string line;
-    while(getline(Flow, line))
-        cout << line << endl;
+    ifstream Flow(Link);
+    char Ch;
+    while(Flow.get(Ch)) //Print the screen
+    {
+        if (Ch=='@') //For the color of Pacman and the fantom
+            Color2(TheColor);
+        else if ( Ch=='O') // For the color of the fantom eyes
+        {
+            Color2(Kwhite);
+            Color2(KBwhite);
+        }
+        cout << Ch;
+        Color2(Kreset);
+    }
 }
 
-unsigned Start_Screen() /*different Start menu option and character animation*/
+unsigned Start_Screen() /*Animation of the Start menu + select of the option*/
 {
     string BeginLink ="../Projet-DUT-C--Pacman/Nos_fichiers/TxtDirectory/StartSheet";
-    LoadScreen(BeginLink+".txt");
+    LoadScreen(BeginLink+".txt", Kyellow);
     usleep(200000);
-    LoadScreen(BeginLink+"2.txt");
+    LoadScreen(BeginLink+"2.txt", Kyellow);
     usleep(200000);
-    LoadScreen(BeginLink+".txt");
+    LoadScreen(BeginLink+".txt", Kyellow);
     usleep(200000);
-    LoadScreen(BeginLink+"2.txt");
+    LoadScreen(BeginLink+"2.txt", Kyellow);
     usleep(200000);
-    LoadScreen(BeginLink+".txt");
+    LoadScreen(BeginLink+".txt", Kyellow);
     usleep(200000);
-    LoadScreen(BeginLink+"2.txt");
+    LoadScreen(BeginLink+"2.txt", Kyellow);
     usleep(200000);
-    LoadScreen(BeginLink+".txt");
+    LoadScreen(BeginLink+".txt", Kyellow);
     unsigned Choice;
     while(true)
     {
@@ -65,15 +85,15 @@ unsigned Start_Screen() /*different Start menu option and character animation*/
 unsigned End_Screen() /*different End menu option and Gost animation*/
 {
     string BeginLink ="../Projet-DUT-C--Pacman/Nos_fichiers/TxtDirectory/EndSheetsDr/EndSheet";
-    LoadScreen(BeginLink+".txt");
+    LoadScreen(BeginLink+".txt", Kred);
     usleep(200000);
-    LoadScreen(BeginLink+"2.txt");
+    LoadScreen(BeginLink+"2.txt", Kred);
     usleep(200000);
     while(true)
     {
         for (unsigned i{3}; i <= 14; i++)
         {
-            LoadScreen(BeginLink+to_string(i)+".txt");
+            LoadScreen(BeginLink+to_string(i)+".txt", Kred);
             usleep(200000);
         }
      }
